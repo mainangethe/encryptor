a714e7b03f638d89439148b13020952bfc46231d
Ng'ethe Maina <ngethe.maina@tezzasolutions.com>
2019-03-26 18:12:31 +0300

    Add File Encryption & Decryption functions
    Add 2 methods that read a file and either encrypt/decrypt it based on the function call.
    
    JIRA: BEDT-230

--- encryptor.rb
+++ encryptor.rb
@@ -22,4 +22,33 @@ class Encryptor
     results.join('')
   end
 
+  def encrypt_file(filename, rotation)
+    # 1. Create the file handle to the input file
+    input = File.open(filename, 'r')
+    # 2. Read the text of the input file
+    file_contents = input.read
+    # 3. Encrypt the text
+    encrypted = encrypt(file_contents, rotation)
+    # 4. Create a name for the output file
+    output_file = 'enc_' + filename
+    # 5. Create an output file handle
+    output = File.open(output_file, 'w')
+    # 6. Write out the text
+    output.write encrypted
+    # 7. Close the file
+    output.close
+    puts "File encrypted. Go open #{ output_file }"
+  end
+
+  def decrypt_file(filename, rotation)
+    input = File.open(filename, 'r')
+    file_contents = input.read
+    decrypted = decrypt(file_contents, rotation)
+    output_file = 'dec_' + filename
+    output = File.open(output_file, 'w')
+    output.write decrypted
+    output.close
+    puts "File decrypted. Go open #{ output_file}"
+  end
+
 end
